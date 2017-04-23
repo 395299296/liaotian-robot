fdee2d64771fdc552206726d5b7484c7ca417c76
hincool <806796291@qq.com>
2017-04-23 11:36:07 +0800

    修改逗号

--- app/main/weather.py
+++ app/main/weather.py
@@ -8,7 +8,7 @@ class Weather():
 
 	def getContent(self, content):
 		d = path.dirname(__file__)
-		with open(path.join(d， 'city.json'), 'r', encoding="utf-8") as json_file:
+		with open(path.join(d, 'city.json'), 'r', encoding="utf-8") as json_file:
 			data = json.load(json_file)
 			city = 'shenzhen'
 			for x in data:
